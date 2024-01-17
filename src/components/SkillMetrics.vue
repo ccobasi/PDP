1560100150dbae052d415654631b939af904c391
Chukwuma Obasi <ccobasi8@gmail.com>
2024-01-17 16:33:47 +0100

    Add gaps table

--- src/components/SkillMetrics.vue
+++ src/components/SkillMetrics.vue
@@ -1,6 +1,25 @@
 <script setup>
   import TableTwo from '../components/Tables/TableTwo.vue'
+  import { ref, computed } from 'vue'
+  import {useSkillsStore} from "@/store/skills"
+
+
+const store = useSkillsStore();
+const skills = store.skills;
+
+
   
+  
+  const skillsPerPage = 10; 
+const currentPage = ref(1);
+
+const totalPages = computed(() => Math.ceil(skills.length / skillsPerPage));
+
+const paginatedItems = computed(() => {
+  const startIndex = (currentPage.value - 1) * skillsPerPage;
+  const endIndex = startIndex + skillsPerPage;
+  return skills.slice(startIndex, endIndex);
+});
   
 </script>
 <template>
@@ -8,12 +27,22 @@
 
     <form action="">
       <div class="d-goal">
-        Do you have a full understanding of your skills metrics?
-        <select class="form-select" aria-label="Default select example">
-          <option class="opt" selected>Yes</option>
-          <option class="opt" value="1">Yes</option>
-          <option class="opt" value="2">No</option>
-        </select>
+        <div class="container">
+
+          <ul class="responsive-table">
+            <li class="table-header">
+              <div class="col col-1">Skill</div>
+              <div class="col col-2">Gaps</div>
+
+            </li>
+            <li class="table-row" v-for="(skill, index) in paginatedItems" :key="index">
+              <div class="col col-1" data-label="Skill">{{ skill.skill }}</div>
+              <div class="col col-2" data-label="Gaps">{{ skill.gap }}</div>
+            </li>
+
+          </ul>
+          <v-pagination v-model="currentPage" :length="totalPages"></v-pagination>
+        </div>
       </div>
       <div class="d-flex justify-content-between mt-4  ">
         <div class="custom-table table-responsive-sm">
@@ -28,6 +57,77 @@
 </template>
 
 <style scoped>
+body {
+  font-family: 'lato', sans-serif;
+}
+.container {
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+h2 {
+  font-size: 26px;
+  margin: 20px 0;
+  text-align: center;
+}
+h2 small {
+  font-size: 0.5em;
+}
+.responsive-table li {
+  border-radius: 3px;
+  padding: 15px 30px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+}
+.responsive-table .table-header {
+  background-color: #47b65c;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 0.03em;
+  color: #fff;
+}
+.responsive-table .table-row {
+  background-color: #fff;
+  box-shadow: 0px 0px 9px 0px rgba(0, 0, 0, 0.1);
+}
+.responsive-table .col-1 {
+  flex-basis: 10%;
+}
+.responsive-table .col-2 {
+  flex-basis: 40%;
+}
+.responsive-table .col-3 {
+  flex-basis: 25%;
+}
+.responsive-table .col-4 {
+  flex-basis: 25%;
+}
+@media all and (max-width: 767px) {
+  .responsive-table .table-header {
+    display: none;
+  }
+  .responsive-table li {
+    display: block;
+  }
+  .responsive-table .col {
+    flex-basis: 100%;
+  }
+  .responsive-table .col {
+    display: flex;
+    padding: 10px 0;
+  }
+  .responsive-table .col:before {
+    color: #47b65c;
+    padding-right: 10px;
+    content: attr(data-label);
+    flex-basis: 50%;
+    text-align: right;
+  }
+}
+
 .intro h3 {
   font-size: 20px;
   font-weight: 600;
